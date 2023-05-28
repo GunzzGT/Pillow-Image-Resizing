ab8b4a48a6277d173ddb17701ffaff825494fc7c
GunzzGT <gunawantandy@gmail.com>
2023-05-28 16:48:12 +0700

    WIP
     its still nothing for now

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # Importing Image class from PIL module
 from PIL import Image
-import io
+import os
 # Opens a image in RGB mode
-im = Image.open(r"C:\Users\gunaw\Pictures\ARMSTROOOOOOOOOOOOONG.jpg")
 
-width, height = im.size
+imglist = []
 
-ratio = im.size
-print(ratio)
+for (root, dirs, files) in os.walk('.', topdown=True):
+    print(root)
+    print(dirs)
+    print(files)
+    imglist.append(files)
 
-im1 = im.resize((256,256))
-im1.save("test.png", format='PNG', dpi=(300, 300))
-im1.show()
+print(imglist)
+
+for x in imglist:
+    im = Image.open(x)
+    width, height = im.size
+    ratio = im.size
+    print(ratio)
+    im1 = im.resize((256, 256))
+    im1.save("test.png", format='PNG', dpi=(300, 300))
+    im1.show()
