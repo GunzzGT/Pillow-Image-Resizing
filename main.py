1c691497e14a834aec91e26c33ecee9131369f9e
GunzzGT <gunawantandy@gmail.com>
2023-05-29 09:52:45 +0700

    Update main.py
    exploring options to read files in directory

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-# Importing Image class from PIL module
 from PIL import Image
 import os
-# Opens a image in RGB mode
 
-imglist = []
+#r"C:\Users\gunaw\Pictures"
 
-for (root, dirs, files) in os.walk('.', topdown=True):
-    print(root)
-    print(dirs)
-    print(files)
-    imglist.append(files)
+directory_name = "C:\\Users\\gunaw\\Pictures"
+file_list = []
+for (root,dirs,files) in os.walk(directory_name):
+    for file in files:
+        file_list.append(os.path.join(root,file))
 
-print(imglist)
+for f in file_list:
+    print(f)
+
+"""
+#NOTES
+testarr1 = os.listdir()
+print(testarr1)
+#list every files and sub-directory on main directory
+
+testarr2 = [os.path.abspath(x) for x in os.listdir()]
+print (testarr2)
+#list every files and sub-directory with its directory, works weirdly if specify which directory on listdir
+
+cwd = ""
+onlyfiles = [os.path.join(cwd, f) for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
+print(onlyfiles)
+#list every files with its directory
 
 for x in imglist:
     im = Image.open(x)
@@ -21,3 +35,4 @@ for x in imglist:
     im1 = im.resize((256, 256))
     im1.save("test.png", format='PNG', dpi=(300, 300))
     im1.show()
+"""
